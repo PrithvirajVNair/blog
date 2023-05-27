15f7e94c03ba69969d85b72c7ce442679dc3a639
Prithviraj V Nair <94970860+PrithvirajVNair@users.noreply.github.com>
2023-05-27 21:43:44 +0530

    Add files via upload

---
+++ src/components/Open.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const Open = () => {
+  return (
+    <div style={{paddingTop:'100px'}}>
+        <Typography variant='h2' color='inherit'>Welcome To Blog</Typography>
+    </div>
+  )
+}
+
+export default Open
